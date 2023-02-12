133e07f4be3dc9aaaf296ac1dbc09d827aff5d76
matCh00 <mateusz.chalik.g@gmail.com>
2023-02-12 13:59:15 +0100

    nav

--- portfolio/src/components/nav/Nav.jsx
+++ portfolio/src/components/nav/Nav.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './nav.css';
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai';
+import {BiBook, BiMessageSquareDetail} from 'react-icons/bi';
+import {RiServiceLine} from 'react-icons/ri';
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#');
+
+  const handleClick = (key) => {
+    setActiveNav(key);
+  }
+
+  const checkIsActive = (key) => {
+    if (activeNav === key)
+      return 'active';
+    return '';
+  }
+
   return (
-    <div>Nav</div>
+    <nav>
+
+      <a href="#" 
+        onClick={() => {handleClick('#')}} 
+        className={checkIsActive('#')}
+      ><AiOutlineHome/></a>
+
+      <a href="#about" 
+        onClick={() => {handleClick('#about')}} 
+        className={checkIsActive('#about')}
+      ><AiOutlineUser/></a>
+
+      <a href="#experience" 
+        onClick={() => {handleClick('#experience')}} 
+        className={checkIsActive('#experience')}
+      ><BiBook/></a>
+
+      <a href="#services" 
+        onClick={() => {handleClick('#services')}} 
+        className={checkIsActive('#services')}
+      ><RiServiceLine/></a>
+
+      <a href="#contact" 
+        onClick={() => {handleClick('#contact')}} 
+        className={checkIsActive('#contact')}
+      ><BiMessageSquareDetail/></a>
+
+    </nav>
   )
 }
 
