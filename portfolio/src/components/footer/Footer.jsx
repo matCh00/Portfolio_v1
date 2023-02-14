74ff91e1c04a8fe2a8e1fc416856fd9793564173
matCh00 <mateusz.chalik.g@gmail.com>
2023-02-15 00:24:21 +0100

    footer

--- portfolio/src/components/footer/Footer.jsx
+++ portfolio/src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './footer.css';
+import {FaFacebook} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+
+      <a href="#" className='footer__logo'>Mateusz Chalik</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Streszczenie</a></li>
+        <li><a href="#about">O mnie</a></li>
+        <li><a href="#experience">Doświadczenie</a></li>
+        <li><a href="#services">Usługi</a></li>
+        <li><a href="#portfolio">Projekty</a></li>
+        <li><a href="#contact">Kontakt</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/mateusz.chalik/"><FaFacebook/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Mateusz Chalik. Wszelkie prawa zastrzeżone.</small>
+      </div>
+
+    </footer>
   )
 }
 
