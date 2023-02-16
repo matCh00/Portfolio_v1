3d1e881772aa4e19b2ad661f95b383f8ab97a869
matCh00 <mateusz.chalik.g@gmail.com>
2023-02-16 18:08:15 +0100

    poprawki

--- portfolio/src/components/header/Socials.jsx
+++ portfolio/src/components/header/Socials.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './socials.css';
 import {BsLinkedin, BsGithub} from 'react-icons/bs';
+import {FaFigma} from 'react-icons/fa';
 
 /** skróty do social mediów */
 
@@ -10,6 +11,7 @@ const Socials = () => {
 
       <a href="https://www.linkedin.com/in/mateusz-chalik-559813233/" target='_blank'><BsLinkedin/></a>
       <a href="https://github.com/matCh00" target='_blank'><BsGithub/></a>
+      <a href="https://www.figma.com/files/user/1208096365060815619?fuid=1208096365060815619" target='_blank'><FaFigma/></a>
 
     </div>
   )
