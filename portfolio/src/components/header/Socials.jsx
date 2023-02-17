d67e83a8a260039d62824b4ae6dd5445c6a4ae0a
matCh00 <mateusz.chalik.g@gmail.com>
2023-02-17 20:17:16 +0100

    poprawki

--- portfolio/src/components/header/Socials.jsx
+++ portfolio/src/components/header/Socials.jsx
@@ -9,9 +9,9 @@ const Socials = () => {
   return (
     <div className='socials'>
 
-      <a href="https://www.linkedin.com/in/mateusz-chalik-559813233/" target='_blank'><BsLinkedin/></a>
+      <a href="https://www.linkedin.com/in/mateusz-chalik/" target='_blank'><BsLinkedin/></a>
       <a href="https://github.com/matCh00" target='_blank'><BsGithub/></a>
-      <a href="https://www.figma.com/files/user/1208096365060815619?fuid=1208096365060815619" target='_blank'><FaFigma/></a>
+      {/* <a href="https://www.figma.com/files/user/1208096365060815619?fuid=1208096365060815619" target='_blank'><FaFigma/></a> */}
 
     </div>
   )
